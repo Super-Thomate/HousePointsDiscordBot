69282263c6208d59ca9ee97449242be186e214ef
ThomasPlaindoux <tomplaindoux1@orange.fr>
2018-10-10 14:17:03 +0000

    useless but funny

--- index.js
+++ index.js
@@ -1576,7 +1576,33 @@ addCommand ("reboot", function (args) {
 }) ;
 
 addCommand ("housebot?", function (args) {
-  args.send ("Non.")
+  if (! args.params.length) {
+    args.send ("What was the question again ?") ;
+    return ;
+  }
+  let allAnswers             =
+              [   "It is certain."
+                , "As I see it, yes."
+                , "Reply hazy, try again"
+                , "Don't count on it."
+                , "It is decidedly so."
+                , "Most likely."
+                , "Ask again later."
+                , "My reply is no."
+                , "Without a doubt."
+                , "Outlook good."
+                , "Better not tell you now."
+                , "My sources say no."
+                , "Yes - definitely."
+                , "Yes."
+                , "Cannot predict now."
+                , "Outlook not so good."
+                , "You may rely on it."
+                , "Signs point to yes."
+                , "Concentrate and ask again."
+                , "Very doubtful."
+              ] ;
+  args.send (allAnswers [Math.floor(allAnswers.length*Math.random())]) ;
 }) ;
 
 addCommand ("addrole", function (args) {
