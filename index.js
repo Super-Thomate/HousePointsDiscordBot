79f586693b515de8bedd459fdcf81cd8782a4a68
ThomasPlaindoux <tomplaindoux1@orange.fr>
2019-01-30 17:05:29 +0000

    fix roles

--- index.js
+++ index.js
@@ -740,7 +740,7 @@ client.on ("message", (message) => {
   if(message.author.bot)
     return ;
   console.log(message.author.username + ' : ' + message.content);
-  var galt = /bendor/i ;
+  var galt = /bendur/i ;
   if (galt.test(message.content))
     message.content = "bendor" ;
   // Ignore messages that don't start with prefix
